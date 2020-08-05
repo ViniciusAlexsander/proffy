7a155eeb315d7cb195205b35376dee72c143063b
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2020-08-05 16:57:09 -0300

    Rotas para criação e listagem de conexões

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionControllers';
 
 
 
 const routes = express.Router();
 const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsController();
 
 
 routes.get('/classes', classesControllers.index)
 routes.post('/classes', classesControllers.create)
+
+routes.get('/connections', connectionsControllers.index)
+routes.post('/connections', connectionsControllers.create)
 
 
 
