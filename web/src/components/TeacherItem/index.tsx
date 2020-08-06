f492b4128affb6f317ee2a6508adab5893c55709
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2020-08-06 17:54:34 -0300

    Finalizada a conexão do frontend com o backend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,36 +1,57 @@
-import React from 'react';
+import React, { ReactHTML } from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const  TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+  function createNewConnection() {
+    api.post('connections',{
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item"> 
     <header>
-      <img src="https://avatars0.githubusercontent.com/u/48867230?s=460&u=d40aa41ab03fcd751e486af3b451328aa3b51d48&v=4" alt="Vinícius Alexsander"/>
+      <img src={teacher.avatar}/>
 
       <div>
-        <strong>Vinicius Alexsander</strong>
-        <span>Química</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
 
-    <p>
-      Entusiasta das melhores tecnologias de química avançada.
-      <br/><br/>
-      Apaixonado por explodir coisas em laboratório.
-    </p>
+    <p>{teacher.bio}</p>
 
     <footer>
       <p>
         Preço/hora
-        <strong>R$ 100,00</strong>
+        <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a 
+        target="_blank" 
+        onClick={createNewConnection}
+        href={`https://wa.me/${teacher.whatsapp}`}
+      >
         <img src={whatsappIcon} alt="Whatsapp"/>
         Entrar em contato
-      </button>
+      </a>
     </footer>
   </article>
   )
